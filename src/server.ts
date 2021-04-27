58ebf67f6b93a8ecbc2a2eb7cf71513ce3dd82f1
MarceSena <marcelinosena@outlook.com>
2021-04-27 17:52:01 -0400

    conect bd

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { Application } from 'express';
 import { InittestController } from './controllers/inittest';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.dataBaseSetup();
   }
 
   private setupExpress(): void {
@@ -22,6 +24,19 @@ export class SetupServer extends Server {
     const inittestController = new InittestController();
     this.addControllers([inittestController]);
   }
+  private async dataBaseSetup():Promise<void>{
+    await database.connect();
+  }
+
+  public async close():Promise<void>{
+     await database.close();
+  }
+  public start():void {
+    this.app.listen(this.port, ( ) => {
+      console.info('Server listening of port:' , this.port);
+
+    });
+  } 
 
   public getApp(): Application {
     return this.app;
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,8 +1,8 @@
 import { SetupServer } from "@src/server";
 import supertest from "supertest";
 
-beforeAll(() => {
-  const server = new SetupServer();
-  server.init();
+let server = new SetupServer();
+beforeAll(async() => {
+  await server.init();
   global.testRequest = supertest(server.getApp());
 });
