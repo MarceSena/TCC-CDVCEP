cbb351fe3bed471f96694b1d4b4488864d818e83
MarceSena <marcelinosena@outlook.com>
2021-04-27 15:57:43 -0400

    feat:add prettier

--- src/controllers/inittest.ts
+++ src/controllers/inittest.ts
@@ -1,18 +1,17 @@
-import { Controller, Get } from "@overnightjs/core";
-import { Request, Response } from "express";
+import { Controller, Get } from '@overnightjs/core';
+import { Request, Response } from 'express';
 
 @Controller('inittest')
 export class InittestController {
-
   @Get('')
   public getForecastForgeLoggedUser(_: Request, res: Response): void {
-    res.send([{
-      "test": "hello wolrd",
-      
-    
-    }, {
-      "test": "hello wolrd",
-    }
+    res.send([
+      {
+        test: 'hello wolrd',
+      },
+      {
+        test: 'hello wolrd',
+      },
     ]);
   }
 }
